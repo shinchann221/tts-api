9fed2d9ab94e8b57bee882c7bec2dc02cf9cab27
Paras Jain <parasj221@gmail.com>
2021-09-30 14:58:13 +0530

    added logging

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const textToSpeech = require('@google-cloud/text-to-speech');
 const express = require('express');
 const tts_client = new textToSpeech.TextToSpeechClient();
+const db = require('./initDB');
 
 
 const app = express();
@@ -11,28 +12,31 @@ app.use(express.urlencoded({ extended: true }));
 
 app.get('/', async (req, res) => {
 
-    let text = req.query.text;
-    let lang = req.query.lang;
-    let gender = req.query.gender;
+  let text = req.query.text;
+  let lang = req.query.lang;
+  let gender = req.query.gender;
+  let text_length = text.length();
+  let today = new Date();
 
 
-    const request = {
-        input: { text: text },
-        voice: { languageCode: lang, ssmlGender: gender },
-        audioConfig: { audioEncoding: 'MP3' },
-    };
+  const request = {
+    input: { text: text },
+    voice: { languageCode: lang, ssmlGender: gender },
+    audioConfig: { audioEncoding: 'MP3' },
+  };
 
-    try {
-        let [response] = await tts_client.synthesizeSpeech(request);
-        res.send(response.audioContent).status(200);
-    } catch (error) {
-        res.send(error);
-    }
+  try {
+    let [response] = await tts_client.synthesizeSpeech(request);
+    db.collection('tts-api-log').insertOne({ 'date': today, 'text': text, 'length': text_length, 'lang': lang});
+    res.send(response.audioContent).status(200);
+  } catch (error) {
+    res.send(error);
+  }
 });
 
 
 var server = app.listen(5003, () => {
-    console.log(`Server is running on port 5003.`);
+  console.log(`Server is running on port 5003.`);
 });
 
 module.exports = server;
